3de5d2c2e1820b151d51b02820a14d7f5ca78600
Camilo685 <46032121+Camilo685@users.noreply.github.com>
2026-03-26 00:57:01 -0500

    Add functionality and fixed style

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let container = document.querySelector(".container");
+let reset = document.querySelector(".reset");
 
 function squareCreation(sze = 16){
     for (let i = 0; i < sze * sze; i++){
         let newDiv = document.createElement("div");
         newDiv.style.backgroundColor = "gold";
-        newDiv.style.border = "solid"
-        newDiv.style.borderWidth = "1px"
-        newDiv.style.width = `${80 / sze}vh`;
-        newDiv.style.height = `${80 / sze}vh`;
+        newDiv.style.outline = "1px solid black"
+        newDiv.style.width = `${90 / sze}vh`;
+        newDiv.style.height = `${90 / sze}vh`;
+
+        let backgroundChange = function(e) {
+            let getRandomInt = (max) => Math.floor(Math.random() * max);
+            e.target.style.backgroundColor = `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
+            e.target.removeEventListener("mouseover", backgroundChange);
+        };
+
+        newDiv.addEventListener("mouseover", backgroundChange);
         container.appendChild(newDiv);
     }
 }
+
+reset.addEventListener("click", () => {
+    let newSize = prompt("Enter new side size (Max = 100): ");
+    newSize = (+newSize <= 100) ? +newSize : 100;
+    container.textContent = "";
+    squareCreation(newSize);
+});
 
 squareCreation();
